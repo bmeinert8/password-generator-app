dc5f7c44a3e808ae57b4c49a3e1a05a5177f2c2a
Brian Meinert <brian.meinert1@gmail.com>
2025-05-21 09:22:49 -0400

    small style update to checkbox list. preperation of DOM manipulated elements in JS

---
+++ main.js
@@ -0,0 +1,27 @@
+const passwordInput = document.querySelector('.js-password-input');
+const copyButton = document.querySelector('.js-copy-btn');
+const characterCount = document.querySelector('.js-count-number');
+const characterSlider = document.querySelector('.js-range-slider');
+const checkboxUppercase = document.getElementById('uppercase');
+const checkboxLowercase = document.getElementById('lowercase');
+const checkboxNumbers = document.getElementById('numbers');
+const checkboxSymbols = document.getElementById('symbols');
+const strengthIndicatorContainer = document.querySelector(
+  '.js-strength-container'
+);
+const strengthText = document.querySelector('.js-strength-level');
+const strengthBar = document.querySelectorAll('.js-strength-bar');
+const generateButton = document.querySelector('.js-generate-btn');
+
+console.log(passwordInput);
+console.log(copyButton);
+console.log(characterCount);
+console.log(characterSlider);
+console.log(checkboxUppercase);
+console.log(checkboxLowercase);
+console.log(checkboxNumbers);
+console.log(checkboxSymbols);
+console.log(strengthIndicatorContainer);
+console.log(strengthText);
+console.log(strengthBar);
+console.log(generateButton);
